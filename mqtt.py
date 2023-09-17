28cf69fa38c1ae3816433480d4b0356d1b502c82
Marley <marley@marleyjaffe.com>
2023-09-17 14:58:31 -0700

    restart mqtt attempts

--- mqtt.py
+++ mqtt.py
@@ -3,11 +3,9 @@
 
 #!python3
 import paho.mqtt.client as mqtt  #import the client1
-from paho.mqtt.properties import Properties #future proofing mqtt v5
-from paho.mqtt.packettypes import PacketTypes #future proofing mqtt v5
 import time
 
-def on_connect(client, userdata, flags, rc, properties=None):
+def on_connect(client, userdata, flags, rc):
     if rc==0:
         client.connected_flag=True #set flag
         print("connected OK")
@@ -16,18 +14,14 @@ def on_connect(client, userdata, flags, rc, properties=None):
 
 mqtt.Client.connected_flag=False#create flag in class
 broker="10.10.10.3"
-properties=None #needed for MQTTv3 with script supporting v5
 client = mqtt.Client("gatepi")             #create new instance 
 client.on_connect=on_connect  #bind call back function
 client.loop_start()
 print("Connecting to broker ",broker)
-client.connect(broker, port=1883, keepalive=60, bind_address="")      #connect to broker
+client.connect(broker)      #connect to broker
 while not client.connected_flag: #wait in loop
     print("In wait loop")
     time.sleep(1)
 print("in Main Loop")
-client.subscribe("gate/#")
-
-
 client.loop_stop()    #Stop loop 
 client.disconnect() # disconnect
