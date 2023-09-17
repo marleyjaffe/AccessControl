c19ece4dc73148b35d8c4b30a966f26f48920d53
Marley <marley@marleyjaffe.com>
2023-09-17 16:35:54 -0700

    update for mqtt setup logic need

--- logic.py
+++ logic.py
@@ -8,9 +8,52 @@ Trigger Home Assistant Event (MQTT)
 
 from db_utils import *
 from StreetAutomation import *
+	
+#set pin num variables
+oPIN = 12
+cPIN = 13
+sPIN = 15
+lPIN = 11
 
+def setup():
+	'''
+	Setup Lock object for package drop
+	'''
+	exPkgLck = lock("package", 11)
 
+	'''
+	Setup gate object
+	'''
+	gate = gate("gate1", {"open": oPIN, "close": cPIN, "stop": sPIN})
 
+	'''
+	Setup database connection
+	'''
+	con = db_connect()
+	cur = con.cursor()
+
+def logic(keypad_input):
+	accessLevel = search_code(con, keypad_input)
+
+	if accessLevel == "gate":
+		print("gate open")
+		gate.open()
+	elif accessLevel == "owner":
+		print("owner")
+		gate.personOpen()
+	elif accessLevel == "lock":
+		print("lock open")
+		exPkgLck.open(10)
+	elif accessLevel == "close":
+		print("closing Gate")
+		gate.close()
+	elif accessLevel == "stop":
+		print("stopping gate")
+		gate.stop(2)
+	else:
+		#TODO: flash lights like an angry old man
+		print("no hits found for either access level or code")
+		pass
 
 if __name__ == '__main__' :
 	'''
@@ -28,54 +71,13 @@ if __name__ == '__main__' :
 
 	'''
 
-
-	'''
-	Setup Lock object for package drop
-	'''
-	exPkgLck = lock("package", 11)
-
-	#set pin num variables
-	oPIN = 12
-	cPIN = 13
-	sPIN = 15
-
-	'''
-	create gate object
-	'''
-	gate = gate("gate1", {"open": oPIN, "close": cPIN, "stop": sPIN})
-
-	'''
-	Setup database connection
-	'''
-	con = db_connect()
-	cur = con.cursor()
-
 	try:
 		while True:
 
 			keypad = input("Enter Access Code: ")
-
-			accessLevel = search_code(con, keypad)
-
-			if accessLevel == "gate":
-				print("gate open")
-				gate.open()
-			elif accessLevel == "owner":
-				print("owner")
-				gate.personOpen()
-			elif accessLevel == "lock":
-				print("lock open")
-				exPkgLck.open(10)
-			elif accessLevel == "close":
-				print("closing Gate")
-				gate.close()
-			elif accessLevel == "stop":
-				print("stopping gate")
-				gate.stop(2)
-			else:
-				#TODO: flash lights like an angry old man
-				print("no hits found for either access level or code")
-				pass
+			setup()
+			logic(keypad)
+			
 	finally:
 		gpioCleanup()
 
