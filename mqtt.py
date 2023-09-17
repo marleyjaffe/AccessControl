e69cc8ef4129042a4850917adfdbe55d36d1b607
Marley <marley@marleyjaffe.com>
2023-09-17 15:03:23 -0700

    mqtt subscribe

--- mqtt.py
+++ mqtt.py
@@ -12,10 +12,17 @@ def on_connect(client, userdata, flags, rc):
     else:
         print("Bad connection Returned code=",rc)
 
+def on_message(client, userdata, message):
+    print("message received " ,str(message.payload.decode("utf-8")))
+    print("message topic=",message.topic)
+    print("message qos=",message.qos)
+    print("message retain flag=",message.retain)
+
 mqtt.Client.connected_flag=False#create flag in class
 broker="10.10.10.3"
 client = mqtt.Client("gatepi")             #create new instance 
 client.on_connect=on_connect  #bind call back function
+client.on_message=on_message #attach function to callback
 client.loop_start()
 print("Connecting to broker ",broker)
 client.connect(broker)      #connect to broker
@@ -24,5 +31,5 @@ while not client.connected_flag: #wait in loop
     time.sleep(1)
 print("in Main Loop")
 client.subscribe("gate/#")
-client.loop_stop()    #Stop loop 
-client.disconnect() # disconnect
+#client.loop_stop()    #Stop loop 
+#client.disconnect() # disconnect
