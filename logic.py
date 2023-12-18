79fc219c131806cd14f3caf9e4c4dc8660713b30
marley <github@crazytofuman.com>
2023-12-17 16:42:54 -0800

    working HomeAssistant MQTT connection. Device is created correctly in HA, status updates correctly (no stopped status). FINALLYgit add logic.py mqtt.py git add logic.py mqtt.py !

--- logic.py
+++ logic.py
@@ -7,7 +7,7 @@ Trigger Home Assistant Event (MQTT)
 	-> change to async MQTT to stop blocking when 
 '''
 
-# python 3.6
+# python 3.10
 
 from db_utils import *
 from StreetAutomation import *
@@ -16,7 +16,11 @@ import json
 import logging
 import random
 import time
-from paho.mqtt import client as mqtt_client
+
+from ha_mqtt_discoverable import Settings, DeviceInfo
+from ha_mqtt_discoverable.sensors import Cover, CoverInfo, Button, ButtonInfo
+from paho.mqtt.client import Client, MQTTMessage
+
 	
 #set pin num variables
 O_PIN = 12
@@ -24,106 +28,7 @@ C_PIN = 13
 S_PIN = 15
 L_PIN = 11
 
-# BROKER = '10.10.10.3'
-# PORT = 1883
-# SUB_TOPIC = "accesscontrol/#"
-# DEFAULT_PUB_TOPIC = "accesscontrol/gate"
-# # generate client ID with pub prefix randomly
-# CLIENT_ID = f'python-mqtt-tcp-pub-sub-{random.randint(0, 1000)}'
-# USERNAME = ''
-# PASSWORD = ''
 
-# FIRST_RECONNECT_DELAY = 1
-# RECONNECT_RATE = 2
-# MAX_RECONNECT_COUNT = 12
-# MAX_RECONNECT_DELAY = 60
-
-# FLAG_EXIT = False
-
-# def on_connect(client, userdata, flags, rc):
-#     if rc == 0 and client.is_connected():
-#         print("Connected to MQTT Broker!")
-#     else:
-#         print(f'Failed to connect, return code {rc}')
-
-
-# def on_disconnect(client, userdata, rc):
-#     logging.info("Disconnected with result code: %s", rc)
-#     reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
-#     while reconnect_count < MAX_RECONNECT_COUNT:
-#         logging.info("Reconnecting in %d seconds...", reconnect_delay)
-#         time.sleep(reconnect_delay)
-
-#         try:
-#             client.reconnect()
-#             logging.info("Reconnected successfully!")
-#             return
-#         except Exception as err:
-#             logging.error("%s. Reconnect failed. Retrying...", err)
-
-#         reconnect_delay *= RECONNECT_RATE
-#         reconnect_delay = min(reconnect_delay, MAX_RECONNECT_DELAY)
-#         reconnect_count += 1
-#     logging.info("Reconnect failed after %s attempts. Exiting...", reconnect_count)
-#     global FLAG_EXIT
-#     FLAG_EXIT = True
-
-
-# def connect_mqtt():
-#     client = mqtt_client.Client(CLIENT_ID)
-#     # client.username_pw_set(USERNAME, PASSWORD)
-#     client.on_connect = on_connect
-#     client.connect(BROKER, PORT, keepalive=120)
-#     client.on_disconnect = on_disconnect
-#     return client
-
-
-# def subscribe(client):
-#     def on_message(client, userdata, msg):
-#         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-#         if msg.topic == "accesscontrol/gate/OPEN":
-#             gate.open()
-#         elif msg.topic == "accesscontrol/gate/CLOSE":
-#             gate.close()
-#         elif msg.topic == "accesscontrol/gate/STOP":
-#             gate.stop()
-#         elif msg.topic == "accesscontrol/gate/PERSON":
-#             gate.personOpen()
-#         elif msg.topic == "accesscontrol/lock/OPEN":
-#             exPkgLck.open()
-
-
-#     client.subscribe(SUB_TOPIC)
-#     client.on_message = on_message
-
-#     print(f"Subscribed to {SUB_TOPIC}!")
-
-
-# def publish(client, PUB_TOPIC =DEFAULT_PUB_TOPIC, msg ="test"):
-#     if not FLAG_EXIT:
-#         # msg_dict = {
-#         #     'msg': msg_count
-#         # }
-#         # msg = json.dumps(msg_dict)
-#         msg = "test"
-#         if not client.is_connected():
-#             logging.error("publish: MQTT client is not connected!")
-#             time.sleep(1)
-#         result = client.publish(PUB_TOPIC, msg)
-#         # result: [0, 1]
-#         status = result[0]
-#         if status == 0:
-#             print(f'Send `{msg}` to topic `{PUB_TOPIC}`')
-#         else:
-#             print(f'Failed to send message to topic {PUB_TOPIC}')
-
-
-# def runMQTTSetup():
-#     logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s',
-#                         level=logging.DEBUG)
-#     client = connect_mqtt()
-#     subscribe(client)
-#     client.loop_forever()
 
 def logic(keypad_input):
 	accessLevel = search_code(con, keypad_input)
@@ -131,6 +36,7 @@ def logic(keypad_input):
 	if accessLevel == "gate":
 		print("gate open")
 		gate.open()
+		catt_gate.open()
 	elif accessLevel == "owner":
 		print("owner")
 		gate.personOpen()
@@ -140,13 +46,100 @@ def logic(keypad_input):
 	elif accessLevel == "close":
 		print("closing Gate")
 		gate.close()
+		catt_gate.closed()
 	elif accessLevel == "stop":
 		print("stopping gate")
 		gate.stop(2)
+		catt_gate.stopped()
 	else:
 		#TODO: flash lights like an angry old man
 		print("no hits found for either access level or code")
 		pass
+
+
+
+# Configure the required parameters for the MQTT broker
+mqtt_settings = Settings.MQTT(host="10.10.10.3")
+
+# Define the device. At least one of `identifiers` or `connections` must be supplied
+device_info = DeviceInfo(name="Catt-AccessControl", identifiers="catt-gatepi")
+
+# Define an optional object to be passed back to the callback
+user_data = "Some custom data"
+
+# Information about the cover
+gate_info = CoverInfo(name="catt-gatepi-gate", unique_id="catt-gatepi-gate", device=device_info)
+gate_settings = Settings(mqtt=mqtt_settings, entity=gate_info)
+
+# To receive state commands from HA, define a callback function:
+def gate_callback(client: Client, user_data, message: MQTTMessage):
+	payload = message.payload.decode()
+	if payload == "OPEN":
+		# let HA know that the cover is opening
+		catt_gate.opening()
+		# call function to open cover
+		gate.open()
+		# Let HA know that the cover was opened
+		catt_gate.open()
+	if payload == "CLOSE":
+		# let HA know that the cover is closing
+		catt_gate.closing()
+		# call function to close the cover
+		gate.close()
+		# Let HA know that the cover was closed
+		catt_gate.closed()
+	if payload == "STOP":
+		# call function to stop the cover
+		gate.stop()
+		# Let HA know that the cover was stopped
+		catt_gate.stopped()
+
+# Instantiate the cover
+catt_gate = Cover(gate_settings, gate_callback, user_data)
+
+# Set the initial state of the cover, which also makes it discoverable
+catt_gate.closed()
+
+
+## Package Drop Door
+# Information about the button
+package_info = ButtonInfo(name="catt-gatepi-package", unique_id="catt-gatepi-packageLock", device=device_info)
+package_settings = Settings(mqtt=mqtt_settings, entity=package_info)
+
+# To receive button commands from HA, define a callback function:
+def package_callback(client: Client, user_data, message: MQTTMessage):
+	exPkgLck.open(10)
+
+# Instantiate the button
+catt_package = Button(package_settings, package_callback, user_data)
+
+# Publish the button's discoverability message to let HA automatically notice it
+catt_package.write_config()
+
+
+## Person Gate
+# Information about the button
+gate_person_info = ButtonInfo(name="catt-gatepi-person", unique_id="catt-gatepi-person", device=device_info)
+gate_person_settings = Settings(mqtt=mqtt_settings, entity=gate_person_info)
+
+# To receive button commands from HA, define a callback function:
+def person_callback(client: Client, user_data, message: MQTTMessage):
+	# Update MQTT status as opening
+	catt_gate.opening()
+	# Call gate person open function
+	gate.personOpen()
+	# Update MQTT status as Stopped
+	catt_gate.stopped()
+
+# Instantiate the button
+catt_person = Button(gate_person_settings, person_callback, user_data)
+
+# Publish the button's discoverability message to let HA automatically notice it
+catt_person.write_config()
+
+
+
+
 
 if __name__ == '__main__' :
 	'''
@@ -180,7 +173,6 @@ if __name__ == '__main__' :
 		'''
 		con = db_connect()
 		cur = con.cursor()
-		runMQTTSetup()
 		while True:
 			keypad = input("Enter Access Code: ")
 			logic(keypad)
